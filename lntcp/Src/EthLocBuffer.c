6847db8edf2e968baddc56892845afc5c5d80b03
Dani Guisado <dguisado@hotmail.com>
2017-06-24 23:22:52 +0200

    Repository creation
    Tested and working, only needs some more heavy load tests

---
+++ lntcp/Src/EthLocBuffer.c
@@ -0,0 +1,411 @@
+
+/**
+ *******************************************************************************************
+ * @file        EthLocBuffer.c
+ * @ingroup     EthLocBuffer
+ * @attention
+ *******************************************************************************************
+ */
+
+/*
+ *******************************************************************************************
+ * Standard include files
+ *******************************************************************************************
+ */
+
+#include <inttypes.h>
+#include <string.h>
+#include "UserIo.h"
+#include "ln_buf.h"
+#include "uip_arp.h"
+#include "ln_interface.h"
+#include "EthLocBuffer.h"
+
+/*
+ *******************************************************************************************
+ * Macro definitions
+ *******************************************************************************************
+ */
+
+/* *INDENT-OFF* */
+
+#define ETH_LOC_BUFFER_MAX_LOCONET_TX     20          /**< Max number of tries * LN_TX_RETRIES_MAX !! */
+
+/*
+ *******************************************************************************************
+ * Types
+ *******************************************************************************************
+  */
+
+/*
+ *******************************************************************************************
+ * Variables
+ *******************************************************************************************
+ */
+
+
+static LnBuf      EthLocBufferLocoNet;                /**< Loconet buffer */
+static uint8_t    EthLocBufferTcpContinue;            /**< Process next Loconet message */
+static uint8_t    EthLocBufferTcpIpRocRail;           /**< Index for TcpIp transmit */
+static uint8_t    EthLocBufferUipTimerCounter;        /**< Timer counter for uip handling */
+static uint8_t    EthLocBufferUipArpTimerCounter;     /**< Timer counter for uip Arp handling */
+/* *INDENT-ON* */
+static uint8_t IdenBytes = 0;
+static uint8_t IdenFound = 0;
+static LnBuf        LnTxBuffer;
+
+static uint8_t sendOk=0;
+static uint8_t sendData=0;
+
+static char dataData[60];
+static uint8_t sendDataLen;
+
+const char sentokstring[]="SENT OK \r\n";
+const char digitMap[] = "0123456789abcdef";
+const char receiveString[] = "RECEIVE ";
+
+/*
+ *******************************************************************************************
+ * Routines implementation
+ *******************************************************************************************
+ */
+ 
+ static uint8_t ProcessTCPRxByte(uint8_t offset, uint8_t rxByte)
+{
+	static uint8_t highNibble = 1;
+	static uint8_t loconetByte = 0; 
+	static lnMsg   *LoconetPacket;
+	LN_STATUS                               TxStatus = LN_DONE;
+	uint8_t                                 TxMaxCnt = 0;
+	
+	
+	
+	if (highNibble)
+	{
+		loconetByte = (rxByte - offset) << 4;
+		highNibble = 0;
+		return 0;
+	}
+	else
+	{
+		loconetByte |= (rxByte - offset);
+		highNibble = 1;
+
+		/* Add it to the buffer */
+
+		addByteLnBuf( &LnTxBuffer, loconetByte ) ;
+
+		/* Check to see if we have received a complete packet yet */
+		LoconetPacket = recvLnMsg( &LnTxBuffer ) ;
+		if(LoconetPacket )
+		{			
+			/* Send the packet received via TCP to the LocoNet*/
+			/*LocoNet.send( LoconetPacket ) ;*/				
+			do
+			{
+			   TxStatus = sendLocoNetPacket(LoconetPacket);
+			   TxMaxCnt++;
+			}
+			while ((TxStatus != LN_DONE) && (TxMaxCnt < ETH_LOC_BUFFER_MAX_LOCONET_TX));
+		
+			return 1;
+		}
+		return 0;    
+	}
+}
+
+
+/**
+ *******************************************************************************************
+ * @fn         static void EthLocBufferTransmitGlobalPowerOff(void)
+ * @brief      Transmit Global PowerOff on the Loconet bus.
+ * @return     None
+ * @attention  -
+ *******************************************************************************************
+ */
+static void EthLocBufferTransmitGlobalPowerOff(void)
+{
+   LN_STATUS                               TxStatus = LN_DONE;
+   lnMsg                                   LocoNetSendPacket;
+   uint8_t                                 TxMaxCnt = 0;
+
+   EthLocBufferTcpContinue = 255;
+   EthLocBufferTcpIpRocRail = 255;
+
+   /* Transmit Power Off on Loconet */
+   LocoNetSendPacket.sz.mesg_size = 2;
+   LocoNetSendPacket.data[0] = 0x82;
+   LocoNetSendPacket.data[1] = 0x7D;
+
+   do
+   {
+      TxStatus = sendLocoNetPacket(&LocoNetSendPacket);
+      TxMaxCnt++;
+   }
+   while ((TxStatus != LN_DONE) && (TxMaxCnt < ETH_LOC_BUFFER_MAX_LOCONET_TX));
+
+   /* Blink TCPIP led indicating no connection is present to RocRail */
+   UserIoSetLed(userIoLed4, userIoLedSetOff);
+   UserIoSetLed(userIoLed5, userIoLedSetOff);
+   UserIoSetLed(userIoLed4, userIoLedSetBlink);
+   UserIoSetLed(userIoLed5, userIoLedSetBlink);
+}
+
+
+/**
+ *******************************************************************************************
+ * @fn	      void EthLocBufferInit(void)
+ * @brief      Init the ENC2860 network chip and init the loconet interface.
+ * @return     None
+ * @attention  -
+ *******************************************************************************************
+ */
+
+void EthLocBufferInit(void)
+{
+   /* Init loconet */
+   initLocoNet(&EthLocBufferLocoNet);
+
+   /* Set used variables to initial values */
+   EthLocBufferTcpContinue = 0;
+   EthLocBufferTcpIpRocRail = 255;
+   EthLocBufferUipTimerCounter = 0;
+   EthLocBufferUipArpTimerCounter = 0;   
+   sendOk=0;
+   sendData=0;
+   sendDataLen=0;
+}
+
+/**
+ *******************************************************************************************
+ * @fn	      void EthLocBufferMain(void)
+ * @brief      Verify if data is present in the network interface or locnet <br>
+ *             interface and process it.
+ * @return     None
+ * @attention  -
+ *******************************************************************************************
+ */
+
+void EthLocBufferMain(void)
+{
+	
+   lnMsg  *RxPacket;
+
+	
+	
+   /* If no TCPIP connection discard all received Loconet data */
+   if (EthLocBufferTcpIpRocRail == 255)
+   {			
+        RxPacket = recvLocoNetPacket();
+		if (RxPacket)
+		{
+			UserIoSetLed(userIoLed6, userIoLedSetFlash);
+		}
+		return;
+   }
+   
+    if (EthLocBufferTcpContinue == 1)
+    {		
+		RxPacket = recvLocoNetPacket();
+		if (RxPacket)
+		{				
+			uint8_t Lnlen = getLnMsgSize( RxPacket );			
+			uint8_t MsgIdx = 8;
+			uint8_t Index = 0;
+			
+			UserIoSetLed(userIoLed6, userIoLedSetFlash);
+			
+			memcpy(dataData,"RECEIVE ", 8);
+			
+			for( Index = 0; Index < Lnlen; Index++ )
+			{
+				dataData[MsgIdx++] = (digitMap[(RxPacket->data[Index] >> 4) & 0xF]);
+				dataData[MsgIdx++] =(digitMap[(RxPacket->data[ Index ] & 0xF)]);
+				dataData[MsgIdx++] = ' ';
+			}			
+			MsgIdx--;
+
+			dataData[MsgIdx++] = 0xD;
+			dataData[MsgIdx++] = 0xA;
+			dataData[MsgIdx] = '\0';
+		
+			sendDataLen = MsgIdx;   
+							
+			sendData=1;
+			EthLocBufferTcpContinue = 0;	  
+		}
+	}
+}
+
+/**
+ *******************************************************************************************
+ * @fn         void EthLocBufferTcpRcvEthernet(void)
+ * @brief      The stack uses calls this function to perform network activities.
+ * @return     None
+ *******************************************************************************************
+ */
+
+void EthLocBufferTcpRcvEthernet(void)
+{   
+   //char                                    DebugStr[20];
+
+   if (uip_timedout() || uip_aborted() || uip_closed() || uip_closed())
+   {
+      /* Connection ended */
+      EthLocBufferTransmitGlobalPowerOff();
+	  return;
+   }
+
+   if (uip_connected())
+   {
+      /* If connection to RocRail present allow forwarding of Loconet data from Loconet bus to TCPIP. */
+      EthLocBufferTcpContinue = 1;
+      EthLocBufferTcpIpRocRail = 1;
+	  UserIoSetLed(userIoLed4, userIoLedSetOff);
+      UserIoSetLed(userIoLed5, userIoLedSetOff);	  
+	  return;
+   }
+
+   if (uip_acked())
+   {
+		//SerialTransmit("Ack\r\n");
+		if (sendOk==1)
+		{
+			sendOk=0;
+			UserIoSetLed(userIoLed4, userIoLedSetOff);			
+		}
+		else if (sendData==1)
+		{
+			sendData=0;
+			UserIoSetLed(userIoLed5, userIoLedSetOff);						
+		}
+		
+		if (sendOk==0 && sendData==0)
+		{
+			EthLocBufferTcpContinue = 1;
+		}
+		else
+		{
+			EthLocBufferTcpContinue = 0;
+		}
+		
+   }
+
+   if (uip_rexmit())
+   {
+      /* Retransmit last transmitted data */
+      if (sendOk==1)
+      {
+		 UserIoSetLed(userIoLed4, userIoLedSetOn);		 
+         uip_send(sentokstring, 10);
+		 EthLocBufferTcpContinue = 2;        
+      }
+	  else if (sendData==1)
+	  {	    
+		UserIoSetLed(userIoLed5, userIoLedSetOn);
+		uip_send(dataData, sendDataLen);
+		EthLocBufferTcpContinue = 2;
+	  }
+	  return;
+   }
+
+   if (uip_newdata() && EthLocBufferTcpContinue == 1)
+   {
+      /* Check if received data is Loconet format */
+      if (uip_datalen() != 0)
+      {
+		 /* Convertimos de ASCII a formato de loconet para enviar */
+		uint8_t indice=0;
+		for (indice=0; indice<uip_datalen(); indice++)
+		{
+			switch (uip_appdata[indice])
+			{
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					ProcessTCPRxByte(0x30, (uint8_t)uip_appdata[indice]);
+					break;
+				case 'A':
+				case 'B':
+				case 'C':
+				case 'F':
+					ProcessTCPRxByte(0x37, (uint8_t)uip_appdata[indice]);
+					break;
+
+				case ' ':
+					/* Do nothing */
+					break;
+				case 'S':
+					IdenBytes = 1;
+					if (IdenFound == 1)
+					{
+						IdenFound = 0;
+					}
+					break;
+
+				case 'E':
+					if(IdenFound == 1)
+						ProcessTCPRxByte(0x37, (uint8_t)uip_appdata[indice]);
+					else
+						IdenBytes++;
+					break;
+
+				case 'N':
+					IdenBytes++;
+					break;
+
+				case 'D':
+					if(IdenFound == 1)
+					{
+						ProcessTCPRxByte(0x37, (uint8_t)uip_appdata[indice]);				
+					}
+					else
+					{
+						if(IdenBytes == 3)
+						{
+							IdenFound = 1;
+						}
+						IdenBytes = 0;
+					}
+					break;
+
+				default:
+					if (IdenFound == 1)
+					{
+						IdenFound = 0;
+						IdenBytes = 0;	
+						sendOk=1;
+						EthLocBufferTcpContinue=0;
+					}
+					break;
+			}
+		}
+		
+      }
+   }
+
+   if (uip_poll())
+   {
+      /* If Loconet data present from Loconet bus transmit it... */
+      if (sendOk==1 && EthLocBufferTcpContinue == 0)
+      {
+         //SerialTransmit("Tx\r\n");		 
+		 UserIoSetLed(userIoLed4, userIoLedSetOn);		 
+         uip_send(sentokstring, 10);
+		 EthLocBufferTcpContinue = 2;
+      }
+	  else if (sendData==1 && EthLocBufferTcpContinue == 0)
+	  {	    
+		UserIoSetLed(userIoLed5, userIoLedSetOn);
+		uip_send(dataData, sendDataLen);
+		EthLocBufferTcpContinue = 2;
+	  }
+   }
+}
---
+++ lntcp/Src/TcpIp.c
@@ -0,0 +1,167 @@
+
+/**
+ *******************************************************************************************
+ * @file 		TcpIp.c
+ * @ingroup    TcpIp
+ * @author		Robert Evers                                                      
+ *******************************************************************************************
+ */
+
+/*
+ *******************************************************************************************
+ * Standard include files
+ *******************************************************************************************
+ */
+#include "uip.h"
+#include "nic.h"
+#include "uip_arp.h"
+#include "UserIo.h"
+#include "uip_arp.h"
+#include "enc28j60.h"
+#include "EthLocBuffer.h"
+#include "TcpIp.h"
+
+/*
+ *******************************************************************************************
+ * Macro definitions
+ *******************************************************************************************
+ */
+
+/* *INDENT-OFF* */
+#define TCP_IP_BUF               ((struct uip_eth_hdr *)&uip_buf[0]) /**< uIP buffer location */
+#define TCP_IP_TMR_OVERRUN        2                                  /**< Rounded timer overrun in msec */
+#define TCP_IP_CNT_TIME           14 / TCP_IP_TMR_OVERRUN            /**< uIP update time */
+#define TCP_IP_ARP_CNT_TIME       10000 / TCP_IP_CNT_TIME            /**< Arp update every ~10msec */
+
+/*
+ *******************************************************************************************
+ * Types
+ *******************************************************************************************
+  */
+
+/*
+ *******************************************************************************************
+ * Variables
+ *******************************************************************************************
+ */
+uint8_t TcpIpUipTimerCounter;                   /**< uIP periodic counter. */
+uint16_t TcpIpUipArpTimerCounter;                /**< uIP Arp periodic counter */
+
+/* *INDENT-OFF* */
+/*
+ *******************************************************************************************
+ * Prototypes
+ *******************************************************************************************
+ */
+
+/*
+ *******************************************************************************************
+ * Routines implementation
+ *******************************************************************************************
+ */
+
+/**
+ *******************************************************************************************
+ * @fn         SIGNAL(SIG_OVERFLOW0)
+ * @brief      Timer interrupt.
+ * @return     None
+ * @attention
+ *******************************************************************************************
+ */
+
+SIGNAL(TIMER0_OVF_vect)
+{
+   sei();
+   TcpIpUipTimerCounter++;
+}
+
+/**
+ *******************************************************************************************
+ * @fn	    	void TcpIpInit(void)
+ * @brief   	Init the TCPIP / uIP stack.
+ * @return		None
+ * @attention	
+ *******************************************************************************************
+ */
+
+void TcpIpInit(void)
+{
+   uint8_t                                 IpAddress[4];
+   uint8_t                                 NetMask[4];
+   uint8_t                                 RouterIp[4];
+
+   /* Set up the network interface */
+   UserIoIpSettingsGet(IpAddress, NetMask, RouterIp);
+   nic_init();
+   uip_init(IpAddress, NetMask, RouterIp);
+   uip_arp_init();
+   uip_listen(HTONS(ETH_LOC_BUFFER_BUFFER_TCP_PORT));
+
+   TcpIpUipTimerCounter = 0;
+   TcpIpUipArpTimerCounter = 0;
+
+   TCCR0B = (1 << CS01) | (1<<CS00);
+   TIMSK0 |= (1<<TOIE0);
+}
+
+/**
+ *******************************************************************************************
+ * @fn         void TcpIpMain(void)
+ * @brief      Update the uIP stack.
+ * @return     None
+ * @attention
+ *******************************************************************************************
+ */
+void TcpIpMain(void)
+{
+   uint8_t                                 Index;
+
+   uip_len = nic_poll();
+   if (uip_len == 0)
+   {
+         // If timed out, call periodic function for each connection
+         if (TcpIpUipTimerCounter >= TCP_IP_CNT_TIME)
+         {
+            TcpIpUipTimerCounter = 0;
+            for (Index = 0; Index < UIP_CONNS; Index++)
+            {
+               uip_periodic(Index);
+               if (uip_len > 0)
+               {
+                  uip_arp_out();
+                  nic_send();
+               }
+            }
+
+            /* Call the ARP timer function every ~10 seconds. */
+            if (++TcpIpUipArpTimerCounter >= TCP_IP_ARP_CNT_TIME)
+            {
+               uip_arp_timer();
+               TcpIpUipArpTimerCounter = 0;
+            }
+         }
+   }
+   else                                                                        // packet received
+   {
+      // process an IP packet
+      if (TCP_IP_BUF->type == htons(UIP_ETHTYPE_IP))
+      {
+         uip_arp_ipin();
+         uip_input();
+         if (uip_len > 0)
+         {
+            uip_arp_out();
+            nic_send();
+         }
+      }
+      // process an ARP packet
+      else if (TCP_IP_BUF->type == htons(UIP_ETHTYPE_ARP))
+      {
+         uip_arp_arpin();
+         if (uip_len > 0)
+            nic_send();
+      }
+
+      TcpIpUipTimerCounter = 0;
+   }
+}
